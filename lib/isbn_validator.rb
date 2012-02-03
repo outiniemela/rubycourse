a88a917823d0955c1f7daa9d1d947c06178ef8ad
Outi Niemela <ayelicia@hotmail.com>
2012-02-03 16:58:02 +0200

    tunnin 3 alku, isbn-testauksia, ja ensimmäinen model tehty

--- lib/isbn_validator.rb
+++ lib/isbn_validator.rb
@@ -1,60 +1,34 @@
 class IsbnValidator
-#getteri sekä setteri
-	attr_accessor :isbn
-#getteri
-#	attr_reader :isbn
-#setteri
-#	attr_writer :isbn
-
-#parametrillinen muodostin
-#jos haluaa, että on oletusarvo:
-#	def initialize(isbn= '')
-	def initialize isbn 
-#		puts "initializing" 
-		if isbn.nil? 
-			@isbn= ""
-		else
-			@isbn= cleanup_isbn isbn		
-		end		
-		 	
-	end
-#getteri, attr_accessor hoitaa molemmat, eli ei siksi tarvitse
-#	def isbn
-#		return @isbn
-#	end
-#setteri
-#	def isbn=(isbn)
-#		@isbn= isbn
-#	end
-	
-	def valid?
-		if @isbn.size==10 || @isbn.size==13
-			true
-		else
-			false		
-		end
-	end
-#class-method, joten ei kutsuta new:ta
-	def self.valid_character?(c)
-		return c==1 || c==2 || c==3 || c==4 ||
-			c==5 || c==6 || c==7 || c==8 ||
-			c==9 || c==0 || x=='x'
-	end
-
-#private-komennon jälkeen olevat luokat toimivat vain, jos niitä kutsutaan ohjelman sisällä
-	private
-	
-	def cleanup_isbn isbn
-#		@isbn.downcase.gsub(/-| /, "")
-		isbn= isbn.downcase
-		isbn= isbn.gsub "-", ""
-		isbn= isbn.gsub " ", ""
-#		return isbn
-#ei tarvitse sanoa return, palauttaa aina viimeisen rivin metodista
-		isbn
-	end
-
-#saman klassin sisältä ja muista classeista-> protected
-	protected
-	
+  VALID_CHARACTERS = ['0', '1', '2', '3', '4', '5',
+                      '6', '7', '8', '9', '0', 'x']
+  
+  attr_reader :isbn
+  
+  # This is a comment
+  def initialize(isbn)
+    if isbn.nil?
+      @isbn = ""
+    else
+      @isbn = cleanup_isbn(isbn)
+    end
+  end
+  
+  def valid?
+    if @isbn.size == 10 || @isbn.size == 13
+      @isbn.chars.all? { |c| valid_character?(c) }
+    else
+      return false
+    end
+  end
+  
+  private
+  
+  def valid_character?(c)
+    return VALID_CHARACTERS.include?(c)
+  end
+  
+  def cleanup_isbn(isbn)
+    isbn.downcase.gsub("-", "").gsub(" ", "")
+  end  
+  
 end
